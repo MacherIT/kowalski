c2ef62f1725c55b83782a001f93151d41b85b352
Bruno Calmels <bruno.calmels@macherit.com>
2018-07-27 10:18:41 -0300

    feat: No se puede navegar nada sin estar autenticado.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  resources :cuentas_gastos
-  resources :conceptos_gastos
-  resources :cuentas_sueldos
-  resources :empleados
-  resources :cuentas_propias
-  resources :duenos
   devise_for :users
+  authenticated :user do
+    resources :cuentas_gastos
+    resources :conceptos_gastos
+    resources :cuentas_sueldos
+    resources :empleados
+    resources :cuentas_propias
+    resources :duenos
 
-  concern :paginatable do
-    get "(page/:page)", action: :index, on: :collection, as: ""
+    concern :paginatable do
+      get "(page/:page)", action: :index, on: :collection, as: ""
+    end
+    resources :movimientos, concerns: :paginatable
+
+    mount Sidekiq::Web => "/sidekiq" # monitoring console
+
+    root to: "movimientos#index" # , as: :authenticated_root
   end
-  resources :movimientos, concerns: :paginatable
 
-  mount Sidekiq::Web => "/sidekiq" # monitoring console
-  root "movimientos#index"
+  # root to: "movimientos#index"
+  root to: redirect("/users/sign_in")
+
+  # root "movimientos#index"
   match "*unmatched", to: "errors#route_not_found", via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
