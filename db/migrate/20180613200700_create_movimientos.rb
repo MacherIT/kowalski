dbf6e621a188f6f31887c7ea202ac736daa1cb76
Bruno Calmels <bruno.calmels@macherit.com>
2018-09-03 11:35:17 -0300

    chore(create_movs): Por conflictos con github

--- db/migrate/20180613200700_create_movimientos.rb
+++ db/migrate/20180613200700_create_movimientos.rb
@@ -3,7 +3,7 @@ class CreateMovimientos < ActiveRecord::Migration[5.2]
     create_table :movimientos do |t|
       t.references :cuenta_debito, foreign_key: true
       t.references :cuenta_credito, foreign_key: true
-      t.string :concepto, null: false, default: ""
+      t.string :concepto, null: false, default: " "
       t.date :fecha_supuesta
       t.date :fecha_efectiva
       t.boolean :hecha, null: false, default: true
